12bea216d2530a7bd75810142499b33aa81eaef1
limchangin <dlackddls5@naver.com>
2025-04-23 10:27:55 +0900

    feat: Order 및 Product 엔티티용 레포지토리 생성

---
+++ src/main/java/com/coffee/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.coffee.repository;
+
+import com.coffee.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order,Long> {
+}
---
+++ src/main/java/com/coffee/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.coffee.repository;
+
+import com.coffee.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+}
