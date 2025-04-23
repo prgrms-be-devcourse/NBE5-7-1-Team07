8270cb435364ad673787695deba8749f960dd7ba
limchangin <dlackddls5@naver.com>
2025-04-23 09:47:43 +0900

    feat: 주문 생성을 위한 상품(Product) 도메인 구현

---
+++ src/main/java/com/coffee/domain/Product.java
@@ -0,0 +1,25 @@
+package com.coffee.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productName;
+    private int price;
+}
