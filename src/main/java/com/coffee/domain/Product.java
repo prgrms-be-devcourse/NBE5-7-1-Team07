5e41c712f754787de3016192cf6fe50c55d25759
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2025-04-24 10:42:13 +0900

    feat:entity수정

--- src/main/java/com/coffee/domain/Product.java
+++ src/main/java/com/coffee/domain/Product.java
@@ -4,18 +4,32 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
     private Long id;
+
+    @Getter @Setter
     private String name;
+
+    @Getter @Setter
     private int price;
+
+    @Getter @Setter
     private String description;
+
+    @Getter @Setter
     private String imagePath;
+
+    @Builder
+    private Product(Long id, String name, int price, String description, String imagePath) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.imagePath = imagePath;
+    }
 }
